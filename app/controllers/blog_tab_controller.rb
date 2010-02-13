5242b859f4c639bcc5e63e6451575efed0a1767f
Paris John Sinclair <paris@concolor.rubypanther.com>
2010-02-12 17:45:24 -0800

    add date

--- app/controllers/blog_tab_controller.rb
+++ app/controllers/blog_tab_controller.rb
@@ -4,6 +4,6 @@ class BlogTabController < ApplicationController
 
   def tab
     @rss = RSS::Parser.parse(open("http://gror.blogspot.com/feeds/posts/default").read, false)
-    render :text => "<ul>"+ @rss.items.collect{|i|"<li><h3>%s</h3><br />%s<br /></li>" % [ i.title.content, i.content.content ] }.join("\n") + "</ul>"
+    render :text => "<ul>"+ @rss.items.collect{|i|"<li><h3>%s (%s)</h3><br />%s<br /></li>" % [ i.title.content, i.updated.content, i.content.content ] }.join("\n") + "</ul>"
   end
 end
