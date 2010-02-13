1b36cb4a4dc342ae74ebc4a32dbadc08856efc17
Paris John Sinclair <paris@concolor.rubypanther.com>
2010-02-12 17:25:12 -0800

    arrrrgh

--- app/controllers/blog_tab_controller.rb
+++ app/controllers/blog_tab_controller.rb
@@ -4,5 +4,6 @@ class BlogTabController < ApplicationController
 
   def tab
     @rss = rss = RSS::Parser.parse(open("http://gror.blogspot.com/feeds/posts/default").read, false)
+    render :text => 'arrg'
   end
 end
